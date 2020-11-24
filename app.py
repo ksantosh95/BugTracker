a6004a1769d770149f6e0fadda6fec1b062674b8
ksantosh95 <ksantosh95@users.noreply.github.com>
2020-11-23 23:51:09 -0500

    Setup complete. Backend connected

--- app.py
+++ app.py
@@ -1,11 +1,40 @@
-from flask import Flask
+from flask import Flask, jsonify
+from flask_sqlalchemy import SQLAlchemy
+import os
+
 
 app = Flask(__name__)
+app.config['SQLALCHEMY_DATABASE_URI'] = "postgresql://postgres:postgres@localhost:5432/test"
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+db = SQLAlchemy(app)
+
+from models import Result
 
 
 @app.route('/')
-def index():
-    return 'hello world'
+def insert():
+    try:
+        entry = Result("xyz",987)
+        db.session.add(entry)
+        db.session.commit()
+    except:
+        db.session.rollback()
+        print("error")
+    finally:
+        db.session.close()
+        print("closed")
+        return ""
+
+    
+
+
+@app.route('/temp')
+def retrieve():
+    res = Result.query.all()
+    res = [r.format() for r in res]
+    return jsonify(res)
+
+    
 
 
 if __name__ == '__main__':
---
+++ models.py
@@ -0,0 +1,25 @@
+from app import db
+from sqlalchemy.dialects.postgresql import JSON
+
+
+class Result(db.Model):
+    __tablename__ = 'results'
+
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String())
+    phone = db.Column(db.Integer)
+
+
+    def __init__(self, name, phone):
+        self.name = name
+        self.phone = phone
+
+    def __repr__(self):
+        return '<id {}>'.format(self.id)
+
+    def format(self):
+        return {
+            "id":self.id,
+            "name":self.name,
+            "phone":self.phone
+        }
