6321842c396f07e404765aff5951607a9237380d
ksantosh95 <ksantosh95@users.noreply.github.com>
2020-11-29 00:12:20 -0500

    Finished adding functionalities for User Page 1

--- app.py
+++ app.py
@@ -1,13 +1,16 @@
-from flask import Flask, jsonify, render_template, redirect
+from flask import Flask, jsonify, render_template, redirect, session, url_for
 from flask_sqlalchemy import SQLAlchemy
 import os
-
-
+from os import environ as env
+import constants
+from authlib.integrations.flask_client import OAuth
+from six.moves.urllib.parse import urlencode
 
 app = Flask(__name__)
 app.config['SQLALCHEMY_DATABASE_URI'] = "postgresql://postgres:postgres@localhost:5432/bugtrackerdb"
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 db = SQLAlchemy(app)
+app.secret_key = "asdfsdfsd"
 
 
 @app.after_request
@@ -19,34 +22,64 @@ def after_request(response):
     return response
 
 AUTH0_DOMAIN = 'ksantosh95.us.auth0.com'
-API_AUDIENCE = 'bugTracker'
 AUTH0_CLIENT_ID = 'ui6xQlOy9NVhh31d0w8UvPjSpaMrZKLS'
-AUTH0_CALLBACK_URL= 'http://127.0.0.1:5000/tickets'
+AUTH0_CALLBACK_URL= 'http://127.0.0.1:5000/callback'
+
+AUTH0_CLIENT_SECRET = 'RgE624251yW7O3WBEbYCR3DAB-q0IEk-L-Q1-jqM9_5q7dELbzYvzEbkwUxGayBp'
+AUTH0_BASE_URL = 'https://' + AUTH0_DOMAIN
+AUTH0_AUDIENCE = 'bugTracker'
+
+oauth = OAuth(app)
+
+auth0 = oauth.register(
+    'auth0',
+    client_id=AUTH0_CLIENT_ID,
+    client_secret=AUTH0_CLIENT_SECRET,
+    api_base_url=AUTH0_BASE_URL,
+    access_token_url=AUTH0_BASE_URL + '/oauth/token',
+    authorize_url=AUTH0_BASE_URL + '/authorize',
+    client_kwargs={
+        'scope': 'openid profile email',
+    },
+)
+
+
 
 @app.route("/", methods=["GET"])
-def generate_auth_url():
+def login():
     
-    url = f'https://{AUTH0_DOMAIN}/authorize' \
-            f'?audience={API_AUDIENCE}' \
-            f'&response_type=token&client_id=' \
-            f'{AUTH0_CLIENT_ID}&redirect_uri=' \
-            f'{AUTH0_CALLBACK_URL}'
+    return auth0.authorize_redirect(redirect_uri=AUTH0_CALLBACK_URL, audience=AUTH0_AUDIENCE)
 
-    return redirect(url)
 
+@app.route('/logout')
+def logout():
+    session.clear()
+    params = {'returnTo': url_for('login', _external=True), 'client_id': AUTH0_CLIENT_ID}
+    return redirect(auth0.api_base_url + '/v2/logout?' + urlencode(params))
 
 
 from models.TicketModel import Ticket
 
 from controllers import UserController
 
-@app.route("/createticket")
-def createticket():
-    return render_template('ticketform.html')
 
-@app.route("/tickets")
-def tickets():
-    return render_template('tickets.html')
+
+
+@app.route('/callback')
+def callback_handling():
+    auth0.authorize_access_token()
+    resp = auth0.get('userinfo')
+    userinfo = resp.json()
+    
+    session['profile'] = {
+       'name': userinfo['name'],
+       'nickname' : userinfo['nickname']
+    }
+    return redirect('/tickets')
+
+
+
+
 
 @app.errorhandler(500)
 def error_500(error):
--- controllers/UserController.py
+++ controllers/UserController.py
@@ -1,4 +1,4 @@
-from flask import Flask, jsonify, request, abort, render_template, redirect, url_for
+from flask import Flask, jsonify, request, abort, render_template, redirect, url_for, session
 from app import app, db
 import os, sys
 from datetime import date
@@ -8,19 +8,25 @@ from models.TicketModel import Ticket
 
 
 @app.route('/tickets', methods=['GET'])
-def ticketview():
-    ticket_value = Ticket.query.filter_by(submitter_id = "10").all()
+def get_tickets():
+    userinfo = session.get('profile')
+    ticket_value = Ticket.query.filter_by(submitter_email= userinfo['name']).all()
     ticket_value = [t.json_format() for t in ticket_value]
-    return render_template('tickets.html', ticket = ticket_value)
+    return render_template('tickets.html', ticket = ticket_value, userinfo = userinfo)
 
+
+@app.route("/createticket")
+def create_ticket():
+    return render_template('ticketform.html')
 
 
 @app.route('/ticketsubmit', methods=['POST'])
-def ticketsubmit():
+def submit_ticket():
+    profile = session.get('profile')
     t_title = request.form.get('t_title')
     t_desc = request.form.get('t_desc')
     emp_id = 0
-    submitter_id =10
+    submitter_email = profile['name']
     #p_id = request.form.get('p_id')
     p_id = 1000
     t_priority = request.form.get('t_priority')
@@ -30,14 +36,14 @@ def ticketsubmit():
     t_create_date = today.strftime("%d/%m/%Y")
     t_close_date = ""
    
-    ticket_entry = Ticket(t_title,t_desc,emp_id,submitter_id,p_id,t_priority,t_status,t_type,t_create_date,t_close_date)
+    ticket_entry = Ticket(t_title,t_desc,emp_id,submitter_email,p_id,t_priority,t_status,t_type,t_create_date,t_close_date)
     try:
         ticket_entry.insert()
     except:
         print(sys.exc_info())
         abort(500)
 
-    return redirect(url_for('tickets'))
+    return redirect(url_for('get_tickets'))
     
 
 
