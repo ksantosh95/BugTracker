046d82bfc1a9e4e98777c59444a8062f7492285a
ksantosh95 <ksantosh95@users.noreply.github.com>
2020-12-07 00:26:54 -0500

    heroku commit - Project details added

--- constants.py
+++ constants.py
@@ -1,7 +1,7 @@
 AUTH0_DOMAIN = 'bugtrac.us.auth0.com'
 AUTH0_CLIENT_ID = 'r6I9WfxRdcE3m6XeST6tlHtwiCEKncJB'
-AUTH0_CALLBACK_URL= 'http://127.0.0.1:5000/callback'
+#AUTH0_CALLBACK_URL= 'http://127.0.0.1:5000/callback'
 #HEROKU DEPLOYMENT
-#AUTH0_CALLBACK_URL= 'https://bugtracker-stage.herokuapp.com/callback'
+AUTH0_CALLBACK_URL= 'https://bugtracker-stage.herokuapp.com/callback'
 AUTH0_CLIENT_SECRET = 'KRIcQ3mcZ8YYAoAKObkRkV4QQrvKahUoe5VfnTWMKKHXFsN5SXmEIiOPS2ZrNtU-'
 AUTH0_AUDIENCE = 'bugTrac'
